73bfd9d77eb6dfe0bebd08d7e68af0ffa34c3df8
Phil Curzon <phil@novelfs.org>
2017-07-14 09:20:50 +0100

    box the iterator

--- src/domain.rs
+++ src/domain.rs
@@ -4,9 +4,11 @@ trait Point3 {
     fn from_origin(&self) -> Vector3;
 }
 
+
+
 trait SpatialTree<T : Point3> {
     fn find_closest_point(&self, p : Point3) -> T;
     fn find_closest_point_within_range(&self, p : Point3, range : f64) -> T;
-    fn find_k_nearest_points(&self, p : Point3, k: u32) -> Iterator<Item=T>;
-    fn find_k_nearest_points_within_range(&self, p : Point3, k : u32, range : f64) -> Iterator<Item=T>;
+    fn find_k_nearest_points(&self, p : Point3, k: u32) -> Box<Iterator<Item=T>>;
+    fn find_k_nearest_points_within_range(&self, p : Point3, k : u32, range : f64) -> Box<Iterator<Item=T>>;
 }
