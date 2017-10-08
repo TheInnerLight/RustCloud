456f391bf1bd4a2840a3ceda18ac8a8052360c51
Phil Curzon <phil@novelfs.org>
2017-10-08 21:30:13 +0100

    Lots of refactoring to use associated types. Added hyperplane generation. Started working on find nearest functionality.

--- src/domain.rs
+++ src/domain.rs
@@ -1,27 +1,51 @@
-use na::{DefaultAllocator, Dim, Vector3, VectorN, U2, U3};
+use na::{DefaultAllocator, DimName, VectorN, U2, U3};
 use na::allocator::Allocator;
 
 /// An n-dimensional point with a defined n-vector from the origin
-pub trait NPoint<N : Dim> where DefaultAllocator: Allocator<f64, N> {
-    fn from_origin(&self) -> VectorN<f64, N>;
+pub trait NPoint: Clone
+where
+    DefaultAllocator: Allocator<f64, Self::N>,
+{
+    type N: DimName;
+
+    fn from_origin(&self) -> &VectorN<f64, Self::N>;
+
+    fn sq_distance_to<T : NPoint<N = Self::N>>(&self, pt2 : T) -> f64 {
+        (self.from_origin() - pt2.from_origin()).norm_squared()
+    }
 }
 
 /// A two dimensional point with a defined vector from the origin
-pub type Point2 = NPoint<U2>;
+pub type Point2 = NPoint<N = U2>;
 
 /// A three dimensional point with a defined vector from the origin
-pub type Point3 = NPoint<U3>;
+pub type Point3 = NPoint<N = U3>;
 
-/// A three dimensional plane defined by a point3 and a vector3
-pub struct Plane3 { 
-    position : Vector3<f64>, 
-    normal : Vector3<f64> 
+/// An n-dimensional plane defined by a vector from the origin and a normal vector
+pub struct Hyperplane<N : DimName> 
+where
+    DefaultAllocator: Allocator<f64, N>
+{ 
+    pub position : VectorN<f64, N>, 
+    pub normal : VectorN<f64, N> 
+}
+
+impl<N : DimName> Hyperplane<N> where
+    DefaultAllocator: Allocator<f64, N> 
+{
+    fn distance_to_point<T : NPoint<N = N>> (&self, pt : T) -> f64 {
+        let distance_to_plane_point = pt.from_origin() - &self.position;
+        self.normal.dot(&distance_to_plane_point)
+    }
 }
 
 /// An arbitrary structure of points capable of answering spatial queries
-pub trait SpatialQueryStructure<T : NPoint<U3>> {
-    fn find_closest_point(&self, p : Point3) -> T;
-    fn find_closest_point_within_range(&self, p : Point3, range : f64) -> T;
-    fn find_k_nearest_points(&self, p : Point3, k: u32) -> Box<Iterator<Item=T>>;
-    fn find_k_nearest_points_within_range(&self, p : Point3, k : u32, range : f64) -> Box<Iterator<Item=T>>;
+pub trait SpatialQueryStructure<T : NPoint>
+where
+    DefaultAllocator: Allocator<f64, T::N> 
+{
+    fn find_closest_point<P : NPoint<N = T::N>>(&self, p : P) -> T;
+    fn find_closest_point_within_range<P : NPoint<N = T::N>>(&self, p : P, range : f64) -> T;
+    fn find_k_nearest_points<P : NPoint<N = T::N>>(&self, p : P, k: u32) -> Box<Iterator<Item=T>>;
+    fn find_k_nearest_points_within_range<P : NPoint<N = T::N>>(&self, p : P, k : u32, range : f64) -> Box<Iterator<Item=T>>;
 }
--- src/spatial_trees/mod.rs
+++ src/spatial_trees/mod.rs
@@ -1,83 +1,127 @@
-use na::{DefaultAllocator, Dim};
+use na::{DefaultAllocator};
 use na::allocator::Allocator;
-use domain::NPoint;
+use domain::{Hyperplane, NPoint};
 
 /// A kd tree
-pub struct KdTree<N : Dim>(Box<kd_tree::KdTreeImpl<N>>) where DefaultAllocator: Allocator<f64, N>;
+pub struct KdTree<P : NPoint + Copy>(kd_tree::KdTreeImpl<P>) where DefaultAllocator: Allocator<f64, P::N> ;
 
-impl<N : Dim> KdTree<N> where DefaultAllocator: Allocator<f64, N> {
+impl<P : NPoint + Copy> KdTree<P> where DefaultAllocator: Allocator<f64, P::N> {
     /// Get all the points from the kd-tree
-    fn get_points(self) -> Vec<Box<NPoint<N>>>  {
+    fn get_points(&self) -> Vec<P>  {
         let mut pts = Vec::new();
-        let KdTree(tree) = self;
+        let &KdTree(ref tree) = self;
         kd_tree::add_points_to_vector(tree, &mut pts);
         pts
     }
 
-    fn build<TItem : NPoint<N> + Copy + 'static>(pts : &Vec<TItem>) -> Self where DefaultAllocator: Allocator<f64, N> {
-        KdTree(Box::new(kd_tree::build(pts, 0)))
+    fn build(pts : Vec<P>) -> Self where DefaultAllocator: Allocator<f64, P::N> {
+        KdTree(kd_tree::build(pts, 0))
     }
+
+    fn splitting_hyperplane(&self) -> Option<Hyperplane<P::N>> {
+        let &KdTree(ref tree) = self;
+        kd_tree::splitting_hyperplane(tree)
+    }
+
 }
 
 mod kd_tree {
-    use na::{DefaultAllocator, Dim};
+    use na::{DefaultAllocator, DimName, U1, VectorN};
     use na::allocator::Allocator;
-    use domain::NPoint;
+    use domain::{Hyperplane, NPoint};
 
     /// kd-Tree implementation structure
-    pub(super) enum KdTreeImpl<N : Dim> where DefaultAllocator: Allocator<f64, N> {
-        Node(usize, Box<KdTreeImpl<N>>, Box<NPoint<N>>, Box<KdTreeImpl<N>>),
+    pub(super) enum KdTreeImpl<P : NPoint> where DefaultAllocator: Allocator<f64, P::N> {
+        Node(usize, Box<KdTreeImpl<P>>, P, Box<KdTreeImpl<P>>),
         Empty()
     }
 
     /// mutably enunerate points from a kd tree into a vector
-    pub(super) fn add_points_to_vector<N : Dim>(tree : Box<KdTreeImpl<N>>, pts : &mut Vec<Box<NPoint<N>>>) where DefaultAllocator: Allocator<f64, N> {
-        let unboxed_tree = *tree;
-        match unboxed_tree {
-            KdTreeImpl::Node(_, l, pt, r) => {
-                add_points_to_vector(l, pts);
-                pts.push(pt);
-                add_points_to_vector(r, pts);
+    pub(super) fn add_points_to_vector<P: NPoint>(tree : &KdTreeImpl<P>, pts : &mut Vec<P>) where DefaultAllocator: Allocator<f64, P::N> {
+        if let &KdTreeImpl::Node(_, ref l, ref pt, ref r) = tree {
+            add_points_to_vector(l, pts);
+            pts.push(pt.clone());
+            add_points_to_vector(r, pts);
+        }
+    }
+
+    pub(super) fn build<P: NPoint>(pts: Vec<P>, dim_idx: usize) -> KdTreeImpl<P> where DefaultAllocator: Allocator<f64, P::N>  {
+        if pts.is_empty() {
+            return KdTreeImpl::Empty();
+        }
+
+        // The dimensionality is statically known
+        let next_dim_idx = if dim_idx + 1 > P::N::dim() {
+            0
+        } else {
+            dim_idx + 1
+        };
+
+        let (left, point, right) = split_vec_with_median(pts, dim_idx).unwrap(); // panics on empty vec
+        KdTreeImpl::Node(
+            dim_idx,
+            Box::new(build(left, next_dim_idx)),
+            point,
+            Box::new(build(right, next_dim_idx)),
+        )
+    }
+
+    /// Returns None on an empty vec. Returns empty l/r subtrees if too few elements
+    fn split_vec_with_median<P: NPoint>(mut pts: Vec<P>, dim_idx: usize) -> Option<(Vec<P>, P, Vec<P>)>
+    where
+        DefaultAllocator: Allocator<f64, P::N>,
+    {
+        pts.sort_by(|a, b| unsafe {
+            a.from_origin()
+                .vget_unchecked(dim_idx)
+                .partial_cmp(b.from_origin().vget_unchecked(dim_idx))
+                .unwrap()
+        });
+        if pts.len() >= 3 {
+            let length = pts.len();
+            let mut middle = pts.split_off(length / 2);
+            let right = middle.split_off(1);
+            // `remove` takes the item out of the vec, which is preferable to copying
+            Some((pts, middle.remove(0), right))
+        } else if pts.len() == 2 {
+            let mut middle = pts.split_off(1);
+            Some((pts, middle.remove(0), Vec::new()))
+        } else if pts.len() == 1 {
+            Some((Vec::new(), pts.remove(0), Vec::new()))
+        } else {
+            None
+        }
+
+    }
+
+    pub(super) fn splitting_hyperplane<P : NPoint>(tree : &KdTreeImpl<P>) -> Option<Hyperplane<P::N>> where DefaultAllocator: Allocator<f64, P::N> {
+        match tree{
+            &KdTreeImpl::Node(dim_idx, _, ref pt, _)  => unsafe {
+                let mut normal_vector = vec![0.0; P::N::dim()];
+                normal_vector[dim_idx] = 1.0;
+                let normal = VectorN::from_data_statically_unchecked(DefaultAllocator::allocate_from_iterator(P::N::name(), U1, normal_vector));
+                Some(Hyperplane {position : pt.from_origin().clone(), normal : normal })
             },
-            KdTreeImpl::Empty() => {}
+            &KdTreeImpl::Empty() => None
         }
     }
 
-    fn split_vec_with_median<N : Dim, TItem : NPoint<N> + Copy + 'static>(pts : &Vec<TItem>, dim_idx :usize) -> (Option<Vec<TItem>>, Option<TItem>, Option<Vec<TItem>>) where DefaultAllocator: Allocator<f64, N> {
+    pub(super) fn find_nearest<P : NPoint>(tree : KdTreeImpl<P>, pt : P) -> Option<P> where DefaultAllocator: Allocator<f64, P::N>  {
         unsafe {
-            let mut pts = pts.to_vec();
-            pts.sort_by( |a,b| a.from_origin().get_unchecked(0, dim_idx).partial_cmp(b.from_origin().get_unchecked(0, dim_idx)).unwrap() );
-            if pts.len() >= 3 {
-                let length = pts.len();
-                let mut middle = pts.split_off(length / 2);
-                let right = middle.split_off(1);
-                (Some(pts), Some(middle[0]), Some(right))
-            } else if pts.len() == 2 {
-                let middle = pts.split_off(1);
-                (Some(pts), Some(middle[0]), None)
-            } else if pts.len() == 1 {
-                (None, Some(pts[0]), None)
-            } else{
-                (None, None, None)
-            }
-        }
-    }
-
-    pub(super) fn build<N : Dim, TItem : NPoint<N> + Copy + 'static>(pts : &Vec<TItem>, dim_idx :usize) -> KdTreeImpl<N> where DefaultAllocator: Allocator<f64, N> {
-        match pts.first() {
-            Some(first_pt) => {
-                let next_dim_idx = if dim_idx + 1 >= first_pt.from_origin().ncols() {0} else {dim_idx + 1};
-                match split_vec_with_median(pts, dim_idx) {
-                    (Some(vec1), Some(pt), Some(vec2))  => KdTreeImpl::Node(dim_idx,     Box::new(build(&vec1, next_dim_idx)),   Box::new(pt),     Box::new(build(&vec2, next_dim_idx))),
-                    (Some(vec1), Some(pt), None      )  => KdTreeImpl::Node(dim_idx,     Box::new(build(&vec1, next_dim_idx)),   Box::new(pt),     Box::new(KdTreeImpl::Empty())),
-                    (None,       Some(pt), Some(vec2))  => KdTreeImpl::Node(dim_idx,     Box::new(KdTreeImpl::Empty()),          Box::new(pt),     Box::new(build(&vec2, next_dim_idx))),
-                    (None,       Some(pt), None      )  => KdTreeImpl::Node(dim_idx,     Box::new(KdTreeImpl::Empty()),          Box::new(pt),     Box::new(KdTreeImpl::Empty())),
-                    (None,       None,     None      )  => panic!("Should be impossible to have a first point but still split into (None, None, None)."),
-                    (_      ,    None,     _         )  => panic!("Meaningless result: point must be contained in a kd-tree node.")
+            match tree {
+                KdTreeImpl::Node(dim_idx, l, cur_pt, r) => {
+                    let cur_pt_vec = cur_pt.from_origin();
+                    let pt_vec = pt.from_origin();
+                    let next = if pt_vec.vget_unchecked(dim_idx) < cur_pt_vec.vget_unchecked(dim_idx) { l } else { r };
+                    let nested_nearest = find_nearest(*next, pt.clone());
+                    match nested_nearest {
+                        Some(_) => None,
+                        None => Some(cur_pt.clone())
+                    }
+                },
+                KdTreeImpl::Empty() => {
+                    None
                 }
-            }
-            None => {
-                KdTreeImpl::Empty()
             }
         }
     }
